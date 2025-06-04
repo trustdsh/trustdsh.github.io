5efbbe5e7a0b1e154ae553dfe1364043cfbec2c0
Vaishnav S Menon <vaishnavsm@gmail.com>
2025-06-04 11:38:45 +0530

    reorg footer and header

---
+++ src/configs/pages.ts
@@ -0,0 +1,71 @@
+export const pages = {
+    audit: {
+        title: 'Audit',
+        path: '/product/audit',
+    },
+    secure: {
+        title: 'Secure',
+        path: '/product/secure',
+    },
+    pricing: {
+        title: 'Pricing',
+        path: '/product/pricing',
+    },
+    docs: {
+
+        title: 'Docs',
+        path: '/docs',
+    },
+    blog: {
+        title: 'Blog',
+        path: '/blog',
+    },
+    github: {
+        title: 'GitHub',
+        path: 'https://github.com/trustdsh',
+    },
+    discord: {
+        title: 'Discord',
+        path: '#',
+    },
+    contact: {
+        title: 'Contact',
+        path: '/contact',
+    },
+    support: {
+        title: 'Support',
+        path: '/contact',
+    },
+    careers: {
+        title: 'Careers',
+        path: '/careers',
+    },
+    privacy: {
+        title: 'Privacy',
+        path: '/legal/privacy',
+    },
+    terms: {
+        title: 'Terms',
+        path: '/legal/terms',
+    },
+    cookies: {
+        title: 'Cookies',
+        path: '/legal/cookies',
+    },
+    login: {
+        title: 'Login',
+        path: '/login',
+    },
+    register: {
+        title: 'Get Started',
+        path: '/login',
+    },
+    demo: {
+        title: 'Book a Demo',
+        path: '/demo',
+    },
+    about: {
+        title: 'About',
+        path: '/',
+    }
+}
