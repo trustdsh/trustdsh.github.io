ecdfeab939a2205ec9f80c11217e72bc275e0749
Vaishnav S Menon <vaishnavsm@gmail.com>
2025-06-19 09:18:59 +0530

    change the copy

--- src/configs/pages.ts
+++ src/configs/pages.ts
@@ -1,75 +1,77 @@
 export const pages = {
-    audit: {
-        title: 'Audit',
-        path: '/#audit',
-    },
-    secure: {
-        title: 'Secure',
-        path: '/#secure',
-    },
-    pricing: {
-        title: 'Pricing',
-        path: '/#pricing',
-    },
-    docs: {
-
-        title: 'Docs',
-        path: '/docs',
-    },
-    blog: {
-        title: 'Blog',
-        path: '/blog',
-    },
-    github: {
-        title: 'GitHub',
-        path: 'https://github.com/trustdsh',
-    },
-    discord: {
-        title: 'Discord',
-        path: 'https://discord.gg/uddjvRC2WU',
-    },
-    contact: {
-        title: 'Contact',
-        path: '/contact',
-    },
-    support: {
-        title: 'Support',
-        path: '/contact',
-    },
-    careers: {
-        title: 'Careers',
-        path: '/careers',
-    },
-    privacy: {
-        title: 'Privacy',
-        path: '/legal/privacy',
-    },
-    terms: {
-        title: 'Terms',
-        path: '/legal/terms',
-    },
-    cookies: {
-        title: 'Cookies',
-        path: '/legal/cookies',
-    },
-    // login: {
-    //     title: 'Login',
-    //     path: '/contact',
-    // },
-    register: {
-        title: 'Get Started',
-        path: '/contact',
-    },
-    demo: {
-        title: 'Book a Demo',
-        path: '/contact',
-    },
-    about: {
-        title: 'About',
-        path: '/#about',
-    },
-    license: {
-        title: 'License',
-        path: '/legal/audit-license',
-    },
-}
+  audit: {
+    title: "Audit",
+    path: "/#audit",
+  },
+  secure: {
+    title: "Secure",
+    path: "/#secure",
+  },
+  pricing: {
+    title: "Pricing",
+    path: "/#pricing",
+  },
+  docs: {
+    title: "Docs",
+    path: "/docs",
+  },
+  blog: {
+    title: "Blog",
+    path: "/blog",
+  },
+  github: {
+    title: "GitHub",
+    path: "https://github.com/trustdsh",
+    external: true,
+  },
+  discord: {
+    title: "Discord",
+    path: "https://discord.gg/uddjvRC2WU",
+    external: true,
+  },
+  contact: {
+    title: "Contact",
+    path: "/contact",
+  },
+  support: {
+    title: "Support",
+    path: "/contact",
+  },
+  careers: {
+    title: "Careers",
+    path: "/careers",
+  },
+  privacy: {
+    title: "Privacy",
+    path: "/legal/privacy",
+  },
+  terms: {
+    title: "Terms",
+    path: "/legal/terms",
+  },
+  cookies: {
+    title: "Cookies",
+    path: "/legal/cookies",
+  },
+  // login: {
+  //     title: 'Login',
+  //     path: '/contact',
+  // },
+  register: {
+    title: "Get Started",
+    path: "/contact",
+  },
+  demo: {
+    title: "Book a Demo",
+    path: "https://outlook.office.com/bookwithme/user/3ba3d1ed5ea04bc792a4c7a8534a11eb@trustd.sh/meetingtype/wKA6x2DzrUmcLmD0JxuNIg2?anonymous&ep=mlink",
+    external: true,
+  },
+  about: {
+    title: "Company",
+    path: "/company",
+  },
+  license: {
+    title: "License",
+    path: "/legal/audit-license",
+  },
+};
